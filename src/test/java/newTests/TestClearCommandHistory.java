f0d590bab841168f23af7add25fe29e0edacbe50
hassanyaasin <49949829+hassanyaasin@users.noreply.github.com>
2021-04-22 15:07:39 +0200

    Add files via upload

---
+++ src/test/java/newTests/TestClearCommandHistory.java
@@ -0,0 +1,6 @@
+package org.mal_lang.enterpriselang.test;
+
+import core.Attacker;
+import org.junit.jupiter.api.Test;
+
+public class ClearCommandHistory extends EnterpriseLangTest {
---
+++ src/test/java/newTests/TestForcedAuthentication.java
@@ -0,0 +1,41 @@
+package org.mal_lang.enterpriselang.test;
+
+import core.Attacker;
+import org.junit.jupiter.api.Test;
+
+public class TestForcedAuthentication extends EnterpriseLangTest {
+    private static class forcedAuthentication{
+        public final UserAccount userAccount = new UserAccount("userAccount");
+        public final OS os = new OS("os");
+        public final Computer computer = new Computer("computer");
+
+        public forcedAuthentication(){
+            userAccount.addOs(os);
+            computer.addOs(os);
+        }
+    }
+
+    @Test
+    public void userRights(){
+        var model = new forcedAuthentication();
+
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(model.userAccount.userRights);
+        attacker.attack();
+
+        model.os.forcedAuthentication.assertCompromisedInstantaneously();
+    }
+
+    @Test
+    public void infectedOS(){
+        var model = new forcedAuthentication();
+
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(model.userAccount.userRights);
+        attacker.attack();
+
+        model.os.bruteForce.assertCompromisedInstantaneously():
+        model.os.forcedAuthentication.assertCompromisedInstantaneously();
+        model.os.domainDiscovery.assertCompromisedInstantaneously();
+    }
+}
